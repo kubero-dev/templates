f443c17a9a8e1af76f692733f81ac24bd543ece6
Gianni Carafa <gianni.carafa@srf.ch>
2023-01-26 14:39:45 +0100

    add local dev server

--- index.js
+++ index.js
@@ -1,7 +1,19 @@
 const express = require('express')
 const app = express()
 
+
 app.get('/', (req, res) => res.send('Hello World!'))
+
+if (process.env.NODE_ENV !== 'test') {
+  app.listen(3000, () => console.log('Example app listening on port 3000!'))
+}
+
+/*
+const http = require('http');
+const server = http.createServer(app);
+const port = 3000;
+server.listen(port, () => console.log(`Example app listening on port ${port}!`));
+*/
 
 // export 'app'
 module.exports = app

61d817df6dcd8218d80ab59c2dcf98f0406e48dd
Gianni Carafa <gianni.carafa@srf.ch>
2023-01-29 21:30:17 +0100

    add github data

--- createindex.py
+++ createindex.py
@@ -2,6 +2,7 @@
 import os
 import yaml
 import json
+import requests
 
 # create a new file called index.yaml
 open("index.json", "w+")
@@ -14,8 +15,9 @@ data = {
 for dirname in os.listdir(basedir):
     dir = os.path.join(basedir, dirname)
     filepath = os.path.join(dir, "service.yaml")
+    apppath = os.path.join(dir, "app.yaml")
     # check if app.yaml exists in the directory
-    if os.path.isfile(filepath):
+    if os.path.isfile(filepath) and os.path.isfile(apppath):
         print (filepath)
         # if not, skip to the next directory
         with open(filepath, "r") as service_yaml:
@@ -23,6 +25,25 @@ for dirname in os.listdir(basedir):
             # convert yaml to json
             content = yaml.safe_load(content)
             # write the json to the index.yaml file
+
+            # check if source contains github string
+            if content.get("source").find("github.com") != -1:
+                # replace url with api url
+                apiURL = content["source"].replace("github.com", "api.github.com/repos")
+                # call the api and get the stars
+                apiData = requests.get(apiURL).json()
+                content["stars"] = apiData.get("stargazers_count")
+                content["forks"] = apiData.get("forks_count")
+                content["watchers"] = apiData.get("watchers_count")
+                content["issues"] = apiData.get("open_issues_count")
+                content["last_updated"] = apiData.get("updated_at")
+                content["last_pushed"] = apiData.get("pushed_at")
+                content["created_at"] = apiData.get("created_at")
+                content["size"] = apiData.get("size")
+                content["language"] = apiData.get("language")
+                content["license"] = apiData.get("license").get("name")
+
+
             data.get("services").append(content)
 
 with open("index.json", "a+") as index_json:

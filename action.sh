e768a8b5818d0e91ec630b08bc30b65f9bc8e54b
Gianni Carafa <gianni.carafa@srf.ch>
2025-01-03 10:05:49 +0100

    add commit config

--- action.sh
+++ action.sh
@@ -4,6 +4,8 @@ pip install -r requirements.txt
 ./templatemigration.py
 ./createindex.py
 
+git config --global user.email "Template bot"
+git config --global user.name "template.bot@kubero.dev"
 cp index.json docs/src/components/Templates/index.json
 cd docs
 git add src/components/Templates/index.json

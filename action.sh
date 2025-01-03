f6825669432594d0fe9a25a987dc49f580aa69d9
Gianni Carafa <gianni.carafa@srf.ch>
2025-01-03 09:57:16 +0100

    add docs submodule

--- action.sh
+++ action.sh
@@ -3,3 +3,10 @@
 pip install -r requirements.txt
 ./templatemigration.py
 ./createindex.py
+
+cp index.json docs/src/components/Templates/index.json
+cd docs
+git add src/components/Templates/index.json
+git commit -m "Update index.json"
+git push
+cd ..
